cc61e044081b14fffca9492967ae6b95a1053505
MarcoMorelloDev <doppiam1@hotmail.it>
2026-01-29 18:06:09 +0100

    Feat: Adapter Finder & Layout Refactoring
    - Aggiunto nuovo strumento "Trova Adattatore" per calcolare raccordi (logica Maschio/Femmina).
    - Rifattorizzato il layout della pagina Raccordi: spostati i controlli nell'Header della CollectionView per risolvere i problemi di scroll su schermi piccoli.
    - Ripristinato BindingContext nel code-behind (fix funzionamento bottoni GAS/NPT e analizzatore diametri).
    - Rimossa riga bianca indesiderata a fondo pagina.

--- Views/FittingsPage.xaml.cs
+++ Views/FittingsPage.xaml.cs
@@ -1,16 +1,60 @@
-using MechTools.ViewModels; // Assicurati che ci sia questo using
+﻿using MechTools.ViewModels;
+namespace MechTools.Views;
 
-namespace MechTools.Views
+public partial class FittingsPage : ContentPage
 {
-    public partial class FittingsPage : ContentPage
+    public FittingsPage()
     {
-        public FittingsPage()
-        {
-            InitializeComponent();
+        InitializeComponent();
+        // Assicurati che il BindingContext sia impostato (probabilmente lo fai in MauiProgram.cs o qui)
+         BindingContext = new FittingsViewModel(); 
+    }
 
-            // COLLEGAMENTO MANUALE (Pi� robusto)
-            // Qui creiamo il "cervello" e lo diamo alla pagina
-            BindingContext = new FittingsViewModel();
+    // NUOVO METODO PER IL CALCOLO
+    private void BtnCalcolaRaccordo_Clicked(object sender, EventArgs e)
+    {
+        // Vibrazione feedback
+        try { Vibration.Default.Vibrate(TimeSpan.FromMilliseconds(50)); } catch { }
+
+        try
+        {
+            // Selezioni lato A
+            string genA = PkGenderA.SelectedItem?.ToString() ?? "Maschio";
+            string sizeA = PkSizeA.SelectedItem?.ToString() ?? "1/4\"";
+            string typeA = PkTypeA.SelectedItem?.ToString() ?? "GAS";
+
+            // Selezioni lato B
+            string genB = PkGenderB.SelectedItem?.ToString() ?? "Femmina";
+            string sizeB = PkSizeB.SelectedItem?.ToString() ?? "1/4\"";
+            string typeB = PkTypeB.SelectedItem?.ToString() ?? "GAS";
+
+            // LOGICA DI INVERSIONE (Se ho Maschio, mi serve Femmina)
+            string reqGenA = (genA == "Maschio") ? "Femmina" : "Maschio";
+            string reqGenB = (genB == "Maschio") ? "Femmina" : "Maschio";
+
+            // Determinazione nome tecnico
+            string nomePezzo = "ADATTATORE";
+
+            if (reqGenA == "Maschio" && reqGenB == "Maschio")
+                nomePezzo = "NIPPLO (M-M)";
+            else if (reqGenA == "Femmina" && reqGenB == "Femmina")
+                nomePezzo = "MANICOTTO (F-F)";
+            else
+                nomePezzo = "PROLUNGA/RIDUZIONE (M-F)";
+
+            // Controllo se è una riduzione
+            if (sizeA != sizeB) nomePezzo += " RIDOTTO";
+
+            // Costruzione stringa finale
+            // Esempio: NIPPLO RIDOTTO
+            // M 1/4 GAS - M 3/8 NPT
+            LblRisultatoRaccordo.Text = $"{nomePezzo}\n" +
+                                        $"Lato A: {reqGenA.Substring(0, 1)} {sizeA} {typeA}  ⟷  " +
+                                        $"Lato B: {reqGenB.Substring(0, 1)} {sizeB} {typeB}";
+        }
+        catch (Exception)
+        {
+            LblRisultatoRaccordo.Text = "Seleziona tutti i campi!";
         }
     }
 }
